7a989598ad16e18efc025b70c2e7008e79e0b88e
oscar <oscar.moreno@cti.espol.edu.ec>
2024-10-18 14:44:00 -0500

    Products pagination

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,27 +1,49 @@
+import ProductsPagination from "@/components/products/ProductsPagination";
 import ProductTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
 
-async function getProducts() {
+
+async function productCount() {
+  return await prisma.product.count()
+}
+
+
+async function getProducts(page: number, pageSize: number) {
+  const skip = (page - 1) * pageSize
   const products = await prisma.product.findMany({
+    take: pageSize,
+    skip: skip,
     include: {
       category: true //productos con categoria 
     }
   })
+  //console.log(products)
   return products
 }
 
 export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>> //typescript se encarga de inferir
 
-export default async function ProductsPage() {
+export default async function ProductsPage({ searchParams }: { searchParams: { page: string } }) {
 
-  const products = await getProducts()
+  const page = +searchParams.page || 1
+  const pageSize = 10
+
+  const productsData = getProducts(page, pageSize)
+  const totalProductsData = productCount()
+  // si ambas funciones son independientes, hacer un promise all para ejecutar en paralelo, caso contrario un aawait para cada fun
+  const [products, totalProducts] = await Promise.all([productsData, totalProductsData])
+  const totalPages = Math.ceil(totalProducts / pageSize)
 
   return (
     <>
       <Heading>Administrar productos</Heading>
       <ProductTable
         products={products}
+      />
+      <ProductsPagination
+        page={page}
+        totalPages={totalPages}
       />
     </>
   )
