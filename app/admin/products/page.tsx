f27b607b50e398a9d3e6cf9c2c87ee18f295f12f
oscar <oscar.moreno@cti.espol.edu.ec>
2024-10-17 23:53:45 -0500

    Admin panel

---
+++ app/admin/products/page.tsx
@@ -0,0 +1,19 @@
+import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
+
+async function getProducts() {
+  const products = await prisma.product.findMany()
+  return products
+}
+
+export default async function ProductsPage() {
+
+  const products = await getProducts()
+
+  return (
+    <>
+      <Heading>Administrar productos</Heading>
+
+    </>
+  )
+}
