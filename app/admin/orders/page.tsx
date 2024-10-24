2f2d9b64ffadc9d7a2594df66f3803ff817f8c98
oscar <oscar.moreno@cti.espol.edu.ec>
2024-10-24 00:38:40 -0500

    Create endpoint with SWR and update orders

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,35 +1,46 @@
+"use client"
+import useSWR from "swr";
 import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
-import { prisma } from "@/src/lib/prisma";
+import { OrderWithProducts } from "@/src/types";
 
-async function getPendingOrders() {
-    const orders = await prisma.order.findMany({
-        where: {
-            status: false
-        },
-        include: {
-            orderProducts: {
-                include: {
-                    product: true
-                }
-            }
-        }
+
+
+export default function OrdersPage() {
+
+    const url = '/admin/orders/api'
+    const fetcher = () => fetch(url).then(res => res.json()).then(data => data)
+    const { data, error, isLoading } = useSWR<OrderWithProducts[]>(url, fetcher, {
+        refreshInterval: 60000,
+        revalidateOnFocus: false
     })
+    //const orders = await getPendingOrders()
+    //console.log(JSON.stringify(orders, null, 2))
+    //const refresOrders = async () => {
+    //    "use server"
+    //    revalidatePath('/admin/orders')
+    //}
 
-    return orders
-}
+    if (isLoading) return 'Cargando.. '
 
-export default async function OrdersPage() {
-
-    const orders = await getPendingOrders()
-//console.log(JSON.stringify(orders, null, 2))
-    return (
+    if (data) return (
         <>
             <Heading>Administrar Ordenes</Heading>
 
-            {orders.length ? (
+            {/**
+             * <form action={refresOrders}
+            >
+                <input
+                    type="submit"
+                    value="Actualizar ordenes"
+                    className="bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer"
+                />
+            </form>
+             */}
+
+            {data.length ? (
                 <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
-                    {orders.map(order => (
+                    {data.map(order => (
                         <OrderCard
                             key={order.id}
                             order={order}
